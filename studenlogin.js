fe7221462084ebe6fa806d2d439ee4484e3dd038
Ivan-Mayabi <mayabivan@gmail.com>
2025-06-18 11:10:58 +0300

    style: package student login
    Description:
    - Placed all the student login files in a single directory.

--- studenlogin.js
+++
@@ -1,9 +0,0 @@
-
-const toggle = document.getElementById("togglePassword");
-const passwordField = document.getElementById("password");
-
-toggle.addEventListener("change", function () {
-    //toggle the type attribute
-    const type = passwordField.type === 'password' ? 'text' : 'password';
-    passwordField.type = type;
-});
