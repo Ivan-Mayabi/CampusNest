976d69b08b6913ec636a8d5229a1f6abba406b8c
Ivan-Mayabi <mayabivan@gmail.com>
2025-07-10 14:40:48 +0300

    Fix: Adding Rooms
    - Allow users to add rooms

--- Desktop28/registerroom.php
+++ Desktop28/registerroom.php
@@ -3,30 +3,42 @@ session_start();
 require_once '../connection.php'; // adjust path if needed
 
 // Make sure landlord is logged in
-if (!isset($_SESSION['landlord_id'])) {
-    header("Location: login.php");
+if (!isset($_SESSION['user_id'])) {
+    header("Location: ../Login/studentlogin.html");
     exit();
 }
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+$houseid = $_GET["houseid"];
+
+
     $roomName = $_POST['roomName'];
     $price = $_POST['price'];
     $availability = $_POST['availability'];
-    $landlordId = $_SESSION['landlord_id'];
+    if($availability=="available"){
+        $roomavailability=1;
+    }
+    else{
+        $roomavailability=0;
+    }
+    $landlordId = $_SESSION['user_id'];
+    print_r($_FILES);
+    $imgData = file_get_contents($_FILES["roomPhoto"]["tmp_name"]);
 
     // ✅ Insert room for this landlord
-    $stmt = $conn->prepare("INSERT INTO rooms (landlord_id, name, price, availability) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("isss", $landlordId, $roomName, $price, $availability);
+    $stmt = $conn->prepare("INSERT INTO room (roomname, roomprice, roomavailability,houseid,roomphoto) VALUES (?, ?, ?, ?,?)");
+    $stmt->bind_param("sssib", $roomName, $price, $roomavailability,$houseid,$imgData);
+    $stmt->send_long_data(4,$imgData);
+
 
     if ($stmt->execute()) {
         echo "✅ Room added successfully.";
-        // Optionally redirect:
-        // header("Location: landlord-dashboard.php?success=1");
+        header("Location: ../Desktop26/Desktop26.php");
+        exit;
     } else {
         echo "❌ Error: " . $stmt->error;
     }
 
     $stmt->close();
     $conn->close();
-}
+
 ?>
