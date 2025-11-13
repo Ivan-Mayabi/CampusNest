4fdda3febe6991d38b986677d70568941b8aa4a5
Ivan-Mayabi <mayabivan@gmail.com>
2025-11-13 12:56:45 +0300

    feat: cloud database

---
+++ connection.php
@@ -0,0 +1,21 @@
+<?php
+// 1. MySQLi extension-for beginners other method requires data objects which advanced users can use
+
+
+// Database connection settings
+//variables to connect to the database
+    $db_server = "campusnest.mysql.database.azure.com:3306";
+    $db_user="root";
+    $db_password = "rootroot";
+    $db_name="db_webappdev_student_accomodation";
+    $conn = "";
+
+    // Create a connection to the MySQL database
+    $conn = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+
+    // Check if the connection was successful
+    if (!$conn) {
+         die("Connection failed: " . mysqli_connect_error());
+    }
+    
+?>
